ebb794b3f7802c8830daafdab03826c513bfcee3
Lycea <Jassi11@gmx.de>
2017-01-26 13:04:15 +0100

    --
    -> added complete interface
    -> added basic methodes for controler

--- CInterface.java
+++ CInterface.java
@@ -1,5 +1,7 @@
 package v1;
 
+
+import java.util.ArrayList;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 //import java.awt.EventQueue;
@@ -12,6 +14,8 @@ import javax.swing.border.EmptyBorder;
  
 public class CInterface extends JFrame implements ActionListener
 {
+    private ArrayList<JButton> buttons = new ArrayList<JButton>();
+    
    //Dialogs
     private JPanel pBasePane;
     private JPanel pMenue;
@@ -19,29 +23,27 @@ public class CInterface extends JFrame implements ActionListener
     private JPanel pOptions;
     
     
-    //Menue dialog controls
-    private JButton bSinglplayer; //opens singleplayer game
-    private JButton bMultiplayer; //opens multiplayer game
-    private JButton bOptions;     //opens option 
-    private JButton bExit;        //exits the game
-    
-    
-    
     //Game dialog controls
-    private JButton bRestart;
     private JLabel  lGameInfo;
     
-    private JButton bTopLeft;
-    private JButton bTopMiddle;
-    private JButton bTopRight;
-    private JButton bMidLeft;
-    private JButton bMidMid;
-    private JButton bMidRight;
-    private JButton bBotLeft;
-    private JButton bBotMid;
-    private JButton bBotRight;
+    
+    //Options dialog controls
+    //private JButton bBack;
+    
+    private JTextField txtNameP1;
+    private JTextField txtNameP2;
+    
+    private JTextField txtSymbP1;
+    private JTextField txtSymbP2;
 
+    private JLabel lNameP1;
+    private JLabel lNameP2;
+    
+    private JLabel lSymbP1;
+    private JLabel lSymbP2;
 
+    
+    CSteuerung steuerung = new CSteuerung(this);
     /**
      * Create the frame.
      */
@@ -51,33 +53,27 @@ public class CInterface extends JFrame implements ActionListener
         //Frame settings
         this.setTitle("Tic-Tac-Toe");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setBounds(100, 100, 450, 300);
+        this.setBounds(100, 100, 400, 300);
+        this.setResizable(false);
+        
         
         
         // create pane objects
         pBasePane = new JPanel();
-        pMenue = new JPanel();
-        pBoard = new JPanel();
+        
+        
         pOptions = new JPanel();
         
         
-        create_basePane(pBasePane);
-        create_Pane(pMenue);
-        create_Pane(pBoard);
-        create_Pane(pOptions);
+        createBasePane(pBasePane);
         
         
-        //create button objects
-        bSinglplayer = create_button(20, 10, 100, 20, "Singleplayer");
+        createPane(pOptions);
         
         
-        
-        bSinglplayer.addActionListener(this);
-      
-        
-        pMenue.add(bSinglplayer);
-        
-        
+        initMenue();
+        initBoard();
+        initOptions();
         
         
         setContentPane(pBasePane);
@@ -86,10 +82,16 @@ public class CInterface extends JFrame implements ActionListener
         pBasePane.add(pMenue, "Menue");
         pBasePane.add(pBoard, "Board");
         pBasePane.add(pOptions, "Options");
+        
+        for (JButton jButton : buttons)
+        {
+            jButton.addActionListener(this);
+        }
+        
          
     }
     
-    private JButton create_button(int x,int y,int w,int h,String name)
+    private JButton createButton(int x,int y,int w,int h,String name)
     {
         JButton tempButton = new JButton(name);
         tempButton.setBounds(x, y, w, h);
@@ -97,14 +99,138 @@ public class CInterface extends JFrame implements ActionListener
         return tempButton;
     }
     
-    private void create_basePane(JPanel panel)
+    private JLabel createLable(int x,int y,int w,int h,String name)
+    {
+        JLabel tempButton = new JLabel(name);
+        tempButton.setBounds(x, y, w, h);
+        
+        return tempButton;
+    }
+    
+    private void initMenue()
+    {
+        int x = 150;
+        int y = 10;
+        int width = 100;
+        int height = 20;
+        int space_between = 30;
+        
+        
+        pMenue = new JPanel();
+        createPane(pMenue);
+        
+        
+        //create button objects
+        
+        buttons.add(createButton(x, y,      width, height, "Singleplayer")); 
+        buttons.add(createButton(x, y+1*30, width, height, "Multiplayer"));
+        buttons.add(createButton(x, y+2*30, width, height, "Options"));
+        buttons.add(createButton(x, y+3*30, width, height, "Exit"));
+        
+        //Listener added
+        
+        //added Buttons to pane
+        pMenue.add(buttons.get(0));
+        pMenue.add(buttons.get(1));
+        pMenue.add(buttons.get(2));
+        pMenue.add(buttons.get(3));
+        
+    }
+    
+    
+    private void initBoard()
+    {
+        
+        int x = 100;
+        int y = 40;
+        int width  = 60;
+        int height = 60;
+        int space_between = 0;
+        
+        pBoard = new JPanel();
+        createPane(pBoard);
+        
+        buttons.add(createButton(x, y,          width, height, ""));
+        buttons.add(createButton(x+width, y,    width, height, ""));
+        buttons.add(createButton(x+width*2, y,  width, height, ""));
+        buttons.add(createButton(x, y+height,   width, height, ""));
+        buttons.add(createButton(x+width, y+height,   width, height, ""));
+        buttons.add(createButton(x+width*2, y+height,   width, height, ""));
+        buttons.add(createButton(x, y+height*2, width, height, ""));
+        buttons.add(createButton(x+width, y+height*2, width, height, ""));
+        buttons.add(createButton(x+width*2, y+height*2, width, height, ""));
+        
+        buttons.add(createButton(x,         y+height*3, width*3, 20, "Restart"));
+        buttons.add(createButton(x, y+height*3+height/3,width*3, 20, "Back"));
+        
+        lGameInfo = createLable(100, 0, 90, 20, "test");
+        
+        
+        
+        //added Buttons to pane
+        for(int i = 4;i < 15; i++)
+        {
+            pBoard.add(buttons.get(i));
+        }
+        
+        pBoard.add(lGameInfo);
+
+    }
+    
+    
+    private void initOptions()
+    {
+     int x = 180;
+     int y = 35;
+     int width = 100;
+     int height = 20;
+        
+        buttons.add(createButton(100, 240,180, height, "back"));
+        
+        
+        txtNameP1 = new JTextField(0);
+        txtNameP1.setBounds(x, y, 70, height);
+        
+        txtNameP2 = new JTextField(0);
+        txtNameP2.setBounds(x, y*2, 70, height);
+        
+        txtSymbP1 = new JTextField(0);
+        txtSymbP1.setBounds(x, y*3, 70, height);
+        
+        txtSymbP2 = new JTextField(0);
+        txtSymbP2.setBounds(x, y*4, 70, height);
+        
+        
+        lNameP1 = createLable(x-100, y,   width, height, "Name P1");
+        lNameP2 = createLable(x-100, y*2, width, height,  "Name p2");
+        lSymbP1 = createLable(x-100, y*3, width, height,  "Symbol p1");
+        lSymbP2 = createLable(x-100, y*4, width, height, "Symbol p2");
+        
+        
+        pOptions.add(txtNameP1);
+        pOptions.add(txtNameP2);
+        pOptions.add(txtSymbP1);
+        pOptions.add(txtSymbP2);
+        pOptions.add(lNameP1);
+        pOptions.add(lNameP2);
+        pOptions.add(lSymbP1);
+        pOptions.add(lSymbP2);
+        pOptions.add(buttons.get(15) );
+        
+        
+        
+        
+    }
+    
+    
+    private void createBasePane(JPanel panel)
     {
         panel.setBorder(new EmptyBorder(5, 5, 5, 5));
         panel.setLayout(new CardLayout(0, 0));
     }
     
     
-    private void create_Pane(JPanel panel)
+    private void createPane(JPanel panel)
     {
         panel.setBorder(new EmptyBorder(5, 5, 5, 5));
         panel.setLayout(null);
@@ -115,10 +241,102 @@ public class CInterface extends JFrame implements ActionListener
     //has to be public not able to change this
     public void actionPerformed (ActionEvent ae)
     {
-        if(ae.getSource() ==this.bSinglplayer )
+        
+        Object object = ae.getSource();
+        CardLayout cl = (CardLayout)(pBasePane.getLayout());
+        
+        switch(buttons.indexOf(object))
         {
-            CardLayout cl = (CardLayout)(pBasePane.getLayout());
-            cl.show(pBasePane, "Board");
+            case 0:
+                steuerung.initSingleplayer();
+                cl.show(pBasePane, "Board");
+                break;
+            case 1:
+                steuerung.initMultiplayer();
+                cl.show(pBasePane, "Board");
+                break;
+            
+            case 2:
+                cl.show(pBasePane, "Options");
+                break;
+                
+            case 3:
+                System.exit(0);
+                break;
+     
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+                steuerung.playerMadeMove(buttons.indexOf(object)-5);
+                break;
+                
+            case 13:
+                steuerung.resetGame();
+                resetGame();
+                break;
+                
+            case 14:
+                cl.show(pBasePane, "Menue");
+                break;
+                
+            case 15:
+                cl.show(pBasePane, "Menue");
+                steuerung.optionsChanged();
+                break;
         }
+    }
+    
+    
+    public void setInfo(String info)
+    {
+        lGameInfo.setText(info);
+    }
+    
+    public void changeButton(int id,String title)
+    {
+        buttons.get(id+5).setText(title);
+        buttons.get(id+5).setEnabled(true);
+    }
+    
+    public void resetGame()
+    {
+        for(int i = 0; i< 8;i++)
+        {
+            changeButton(i, " ");
+        }
+    }
+    
+    
+    
+    
+    /**********************************
+     * 
+     * Getter for the option text fields
+     *
+     ************************************/
+    public String getPlayerName1()
+    {
+       return txtNameP1.getText();
+    }
+    
+    public String getPlayerName2()
+    {
+        return txtNameP2.getText();
+    }
+    
+    public String getPlayerSymb1()
+    {
+        return txtSymbP1.getText();
+    }
+    
+    public String getPlayerSymb2()
+    {
+        return txtSymbP2.getText();
     }
 }
--- CSteuerung.java
+++ CSteuerung.java
@@ -2,5 +2,44 @@ package v1;
 
 public class CSteuerung
 {
-
+    CGame game;
+    //COptions o;
+    CInterface intf;
+    
+    
+    public CSteuerung(CInterface pInt)
+    {
+        intf = pInt;
+    }
+    
+    public void resetGame()
+    {
+        
+    }
+    
+    public void playerMadeMove(int fieldnum)
+    {
+        
+    }
+    
+    public void initMultiplayer()
+    {
+        game = new CGame();
+    }
+    
+    public void initSingleplayer()
+    {
+        String info;
+        game = new CGame();
+        
+        intf.setInfo("Player xs turn");
+    }
+    
+    public void optionsChanged()
+    {
+        intf.getPlayerName1();
+        intf.getPlayerName2();
+        intf.getPlayerSymb1();
+        intf.getPlayerSymb2();
+    }
 }
