01de2fbde5cfc2cd31975303da4272647ab69e36
RagnarStefansson <julius.hauck@gmx.de>
2017-01-26 13:10:22 +0100

    Add files via upload

--- COptions.java
+++ COptions.java
@@ -1,12 +1,15 @@
 package v1;
 
+//Settings
 public class COptions {
 	
+	//Variables
 	private String sPlayerName;
 	private String sPlayerName2;
 	private String sPlayerSymbol1;
 	private String sPlayerSymbol2;
 	
+	//get the Name of the Player
     public String getPlayerName(int i_playernum)
     {
     	if(i_playernum == 1)
@@ -18,7 +21,8 @@ public class COptions {
     		return sPlayerName2; 
     	}
     }
-    
+
+	//set the Name of the Player
     public void setPlayerName(int i_playernum, String s_Name)
     {
     	if(i_playernum == 1)
@@ -30,7 +34,8 @@ public class COptions {
     		sPlayerName2 = s_Name;
     	}        
     }
-    
+
+	//get the Symbol of the Player
     public String getPlayerSymbol(int i_player)
     {
     	if(i_player == 1)
@@ -42,7 +47,8 @@ public class COptions {
     		return sPlayerSymbol2; 
     	}    	
     }
-    
+ 
+	//Set the Symbol of the Player
     public void setPlayerSymbol(int i_player, String s_symbol)
     {
     	if(i_player == 1)
